1120fc8c54cee8be828e0ce1f11942070e665356
Grants52 <42580846+Grants52@users.noreply.github.com>
2020-04-28 17:37:39 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ local_registry="http://0.0.0.0:4873"
 
 # start local registry
 tmp_registry_log=`mktemp`
-#sh -c "mkdir -p $HOME/.config/verdaccio"
-sh -c "cp --verbose /config.yaml $HOME/.config/verdaccio/config.yaml"
+sh -c "mkdir -p $HOME/.config/verdaccio"
+#sh -c "cp --verbose /config.yaml $HOME/.config/verdaccio/config.yaml"
 sh -c "nohup verdaccio --config $HOME/.config/verdaccio/config.yaml &>$tmp_registry_log &"
 # wait for `verdaccio` to boot
 # FIXME: this throws a syntax error, but would be great to make it run
